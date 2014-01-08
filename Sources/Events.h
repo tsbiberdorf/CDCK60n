bb8123cb638a3a7aae6cbedd9bf06455f39a9d89
Terry Biberdorf <terry.biberdorf@symcom.com>
2014-01-08 11:13:58 -0700

    working example of CDC code

---
+++ Sources/Events.h
@@ -0,0 +1,129 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : ProcessorExpert
+**     Processor   : MK60DN512ZVLQ10
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2014-01-08, 10:47, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "USB1.h"
+#include "USB0.h"
+#include "CDC1.h"
+#include "Tx1.h"
+#include "Rx1.h"
+#include "AS1.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+#include "LED2.h"
+#include "LEDpin2.h"
+#include "BitIoLdd2.h"
+#include "LED3.h"
+#include "LEDpin3.h"
+#include "BitIoLdd3.h"
+#include "LED4.h"
+#include "LEDpin4.h"
+#include "BitIoLdd4.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT0 (module Events)
+**
+**     Component   :  Cpu [MK60N512LQ100]
+**     Description :
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the <NMI
+**         interrupt> property is set to 'Enabled'.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Cpu_OnNMIINT0(void);
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnBlockReceived (module Events)
+**
+**     Component   :  AS1 [Serial_LDD]
+*/
+/*!
+**     @brief
+**         This event is called when the requested number of data is
+**         moved to the input buffer.
+**     @param
+**         UserDataPtr     - Pointer to the user or
+**                           RTOS specific data. This pointer is passed
+**                           as the parameter of Init method.
+*/
+/* ===================================================================*/
+void AS1_OnBlockReceived(LDD_TUserData *UserDataPtr);
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnBlockSent (module Events)
+**
+**     Component   :  AS1 [Serial_LDD]
+*/
+/*!
+**     @brief
+**         This event is called after the last character from the
+**         output buffer is moved to the transmitter. 
+**     @param
+**         UserDataPtr     - Pointer to the user or
+**                           RTOS specific data. This pointer is passed
+**                           as the parameter of Init method.
+*/
+/* ===================================================================*/
+void AS1_OnBlockSent(LDD_TUserData *UserDataPtr);
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.2 [05.07]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Sources/ProcessorExpert.c
@@ -0,0 +1,148 @@
+/* ###################################################################
+**     Filename    : ProcessorExpert.c
+**     Project     : ProcessorExpert
+**     Processor   : MK60DN512ZVLQ10
+**     Version     : Driver 01.01
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2014-01-08, 10:47, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file ProcessorExpert.c
+** @version 01.01
+** @brief
+**         Main module.
+**         This module contains user's application code.
+*/         
+/*!
+**  @addtogroup ProcessorExpert_module ProcessorExpert module documentation
+**  @{
+*/         
+/* MODULE ProcessorExpert */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "USB1.h"
+#include "USB0.h"
+#include "CDC1.h"
+#include "Tx1.h"
+#include "Rx1.h"
+#include "AS1.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+#include "LED2.h"
+#include "LEDpin2.h"
+#include "BitIoLdd2.h"
+#include "LED3.h"
+#include "LEDpin3.h"
+#include "BitIoLdd3.h"
+#include "LED4.h"
+#include "LEDpin4.h"
+#include "BitIoLdd4.h"
+/* Including shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+#define USE_TWR_SER_UART  0 /* if set to one, it will use the UART on the TWR-SER board. */
+
+static uint8_t cdc_buffer[USB1_DATA_BUFF_SIZE];
+static uint8_t in_buffer[USB1_DATA_BUFF_SIZE];
+static LDD_TDeviceData *serialHandle = NULL; /* handle to device data */
+volatile uint8_t SentFlag = 0; /* this will be set to 1 once the block has been sent */
+
+static void SendChar(unsigned char ch) {
+  SentFlag = 0; /* this will be set to 1 once the block has been sent */
+  while(AS1_SendBlock(serialHandle, (LDD_TData*)&ch, 1)!=ERR_OK) {} /* Send char */
+  while(SentFlag==0) {} /* wait until we get the green flag from the TX interrupt */
+}
+
+static void SendStr(const unsigned char *str) {
+#if USE_TWR_SER_UART
+  while(*str!='\0') {
+    SendChar(*str++);
+  }
+#else
+  (void)str; /* unused */
+#endif
+}
+
+static void CDC_Run(void) {
+  int i;
+
+  SendStr((unsigned char*)"Hello World to Kinetis SCI3!\r\n");
+  for(;;) {
+    while(CDC1_App_Task(cdc_buffer, sizeof(cdc_buffer))==ERR_BUSOFF) {
+      /* device not enumerated */
+      LED1_Neg(); LED2_Off();
+     // WAIT1_Waitms(1); /* just give back some CPU time. */
+      SendStr((unsigned char*)"waiting to enumerate...\r\n");
+    }
+    LED1_Off(); LED2_Neg();
+    if (CDC1_GetCharsInRxBuf()!=0) {
+      i = 0;
+      while(   i<sizeof(in_buffer)-1
+            && CDC1_GetChar(&in_buffer[i])==ERR_OK
+           )
+      {
+        i++;
+      }
+      in_buffer[i] = '\0';
+      (void)CDC1_SendString((unsigned char*)"echo: ");
+      (void)CDC1_SendString(in_buffer);
+      (void)CDC1_SendString((unsigned char*)"\r\n");
+      SendStr((unsigned char*)"echo to SCI3: ");
+      SendStr(in_buffer);
+      SendStr((unsigned char*)"\r\n");
+    } else {
+     // WAIT1_Waitms(5);
+    }
+  }
+}
+
+void main(void)
+{
+  /* Write your local variable definition here */
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  /* Write your code here */
+  serialHandle = AS1_Init(NULL);
+  CDC_Run();
+
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END ProcessorExpert */
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.2 [05.07]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
